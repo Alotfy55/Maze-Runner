4ae1b08fa19e37b2a088a3af2aee94b7891a9822
Ahmed Lotfy <45941854+Alotfy55@users.noreply.github.com>
2019-12-05 22:09:51 +0200

    implmented best first search
    best first search is now implmnted and tested waiting to test on more mazes before merging with master

--- Maze-Runner/Classes.cpp
+++ Maze-Runner/Classes.cpp
@@ -49,9 +49,11 @@ void The_Maze::creating_2D_arrays()
 {
 	Maze = new char* [length];
 	visitedPositions = new bool* [length];
+	parents = new pair<int, int>*[length];
 	for (int i = 0; i < length; i++) {
 		Maze[i] = new char[width];
 		visitedPositions[i] = new bool[width];
+		parents[i] = new pair<int, int>[width];
 	}
 }
 
@@ -78,3 +80,81 @@ void The_Maze::zeroVisitedArray() {
 
 }
 
+double The_Maze::BestFact(pair<int,int> point)
+{
+	double fact = sqrt(pow((endingPoint.first - point.first), 2) + pow((endingPoint.second - point.second), 2));
+	return fact;
+}
+
+void The_Maze::BestFirst() {
+	zeroVisitedArray();
+
+	priority_queue<pair<double, pair<int, int>>> searchQueue;
+	pair<double, pair<int, int>> start;
+	start.first = -1;
+	start.second = startingPoint;
+	visitedPositions[startingPoint.first][startingPoint.second] = true;
+	searchQueue.push(start);
+	parents[startingPoint.first][startingPoint.second] = {-1 , -1};
+	int i = 0;
+	while (!searchQueue.empty())
+	{
+		pair<double, pair <int, int>> x = searchQueue.top();
+		searchQueue.pop();
+		if (x.second == endingPoint)
+		{
+			cout << "found at :: " << (x.second.second) / 2 << " " << (x.second.first) / 2 << endl ;
+			bestTrack(x.second , 0);
+			return;
+		}
+		node point(x.second.first, x.second.second, 0);
+		getChildren(point, searchQueue);
+		
+	}
+}
+
+void The_Maze::bestTrack(pair<int, int> point , int count)
+{
+	pair<int, int> base = { -1,-1 };
+	if (parents[point.first][point.second] == base)
+	{
+		cout << "Path is " << count << " steps long" << endl;
+		cout << "(" << (point.first)/2 << "," << (point.second)/2 << ")" << " ";
+		return;
+	}
+	bestTrack(parents[point.first][point.second], count + 1);
+	cout << "(" << (point.second) / 2 << "," << (point.first) / 2 << ")" << " ";
+	return;
+}
+
+void The_Maze::getChildren(node point, priority_queue<pair<double, pair<int, int>>>& searchingQueue) {
+	// upper cell
+	getChild(point, 0, -1, searchingQueue);
+	//lower cell
+	getChild(point, 0, 1, searchingQueue);
+	//right cell
+	getChild(point, 1, 0, searchingQueue);
+	//left cell
+	getChild(point, -1, 0, searchingQueue);
+}
+
+void The_Maze::getChild(node point, int x, int y, priority_queue<pair<double, pair<int, int>>>& searchingQueue) {
+	if (Maze[point.x + x][point.y + y] == ' ' && !visitedPositions[point.x + (x * 2)][point.y + (y * 2)])
+	{
+		visitedPositions[point.x + (x * 2)][point.y + (y * 2)] = true;
+
+		pair<int, int> newPoint;
+		newPoint.first = point.x + (x * 2);
+		newPoint.second = point.y + (y * 2);
+
+		double DistToEnd = BestFact(newPoint);
+
+		parents[newPoint.first][newPoint.second] = { point.x , point.y };
+
+		pair<double, pair<int, int>> append;
+		append.first = DistToEnd;
+		append.second = newPoint;
+
+		searchingQueue.push(append);
+	}
+}
