22bf4765e4b7599b5b27ae2b03a9783ae00f1b73
omadex <48694415+omadex@users.noreply.github.com>
2019-12-09 02:53:12 +0200

    updated classes.h

--- Maze-Runner/Classes.h
+++ Maze-Runner/Classes.h
@@ -1,6 +1,3 @@
-﻿//
-// Created by ziad on ٢٤‏/١١‏/٢٠١٩.
-//
 
 #ifndef MAZERUNNER_CLASS_H
 #define MAZERUNNER_CLASS_H
@@ -18,33 +15,43 @@ using namespace std;
 struct node
 {
 	int x, y;
-	int DistanceToPoint;
-	node* parent;
+
 	node()
 	{
-		x = 0; y = 0; DistanceToPoint = 0; parent = NULL;
+		x = 0; y = 0;
 	}
-	node(int posx, int posy, int Dist)
+	node(int posx, int posy)
 	{
 		x = posx;
 		y = posy;
-		DistanceToPoint = Dist;
 	}
 };
 
 class The_Maze {
-	int row , coloumn;
+	int length, width;
 	pair <int, int> startingPoint, endingPoint;
 	char** Maze;
 	bool** visitedPositions;
+	pair<int, int>** parents;
 	void creating_2D_arrays();
+	stack<pair<int, int>> s;
+	int dfs_path_ctr = 0;
+	int dfs_steps_ctr = 0;
 public:
 	The_Maze();
 	void takeInput(string);
 	~The_Maze();
 	void printMaze();
 	void zeroVisitedArray();
-	
+	void BFS();
+	void BestFirst();
+	double BestFact(pair<int, int>);
+	void bestTrack(pair<int, int>, int);
+	void getChildren(node, priority_queue<pair<double, pair<int, int>>>&);
+	void getChild(node, int, int, priority_queue<pair<double, pair<int, int>>>&);
+	void bestTrack(node);
+	bool dfs_help(int i , int j);
+	void dfs();
 };
 
 
