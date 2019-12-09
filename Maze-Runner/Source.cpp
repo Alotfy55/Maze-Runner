e5ed05d6f8ffd0c895d71e156eee2f59bd68b4ca
omadex <48694415+omadex@users.noreply.github.com>
2019-12-09 03:03:26 +0200

    Update Source.cpp

--- Maze-Runner/Source.cpp
+++ Maze-Runner/Source.cpp
@@ -6,5 +6,12 @@ int main() {
 	The_Maze maze;
 	maze.takeInput("Maze");
 	maze.printMaze();
+	cout << endl;
+	maze.BestFirst();
+	cout << endl;
+	maze.BFS();
+	cout << endl;
+	maze.dfs();
+	system("pause");
 	return 0;
 }
