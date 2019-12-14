506735aed694ab9449affc4e8ac0250a333eaf6c
omadex <48694415+omadex@users.noreply.github.com>
2019-12-15 01:33:32 +0200

    Update Classes.cpp

--- Maze-Runner/Classes.cpp
+++ Maze-Runner/Classes.cpp
@@ -3,35 +3,31 @@ using namespace std;
 
 The_Maze::The_Maze()
 {
-	length = width = 0;
-
-
+	row = coloumn = 0;
 }
-
-
 
 The_Maze::~The_Maze()
 {
-	/*for (int i = 0; i < length; i++)
+	for (int i = 0; i < row; i++)
 	{
 		delete[] Maze[i];
 		delete[] visitedPositions[i];
-	}*/
+		delete[] parents[i];
+	}
+	delete[] parents;
 	delete[] Maze;
 	delete[] visitedPositions;
 }
 
-
-
 void The_Maze::takeInput(string name)
 {
 	ifstream file(name + ".txt");
-	file >> length >> width;
+	file >> coloumn >> row;
 	file.ignore();
-	length = length * 2 + 2;
-	width = width * 2 + 2;
+	row = row * 2 + 2;
+	coloumn = coloumn * 2 + 2;
 	creating_2D_arrays();
-	for (int i = 0; i < width; i++) {
+	for (int i = 0; i < row; i++) {
 		string maze;
 		getline(file, maze);
 		for (int j = 0; j < maze.length(); j++) {
@@ -46,25 +42,26 @@ void The_Maze::takeInput(string name)
 	}
 }
 
-
 void The_Maze::creating_2D_arrays()
 {
-	Maze = new char*[length];
-	visitedPositions = new bool*[length];
-	parents = new pair<int, int>*[length];
-	for (int i = 0; i < length; i++) {
-		Maze[i] = new char[width];
-		visitedPositions[i] = new bool[width];
-		
-		parents[i] = new pair<int, int>[width];
+	Maze = new char*[row];
+	parents = new pair<int, int>*[row];
+
+	visitedPositions = new bool*[row];
+	for (int i = 0; i < row; i++) {
+		Maze[i] = new char[coloumn];
+		visitedPositions[i] = new bool[coloumn];
+		parents[i] = new pair<int, int>[coloumn];
 	}
 }
 
 void The_Maze::printMaze()
 {
-	for (int i = 0; i < width - 1; i++)
+
+	cout << startingPoint.first << " " << startingPoint.second << endl << endingPoint.first << " " << endingPoint.second << endl;
+	for (int i = 0; i < row - 1; i++)
 	{
-		for (int j = 0; j < length - 1; j++)
+		for (int j = 0; j < coloumn - 1; j++)
 		{
 			cout << Maze[i][j];
 		}
@@ -73,61 +70,68 @@ void The_Maze::printMaze()
 }
 
 void The_Maze::zeroVisitedArray() {
-	for (int i = 0; i < width; i++)
+	for (int i = 1; i < row; i++)
 	{
-		for (int j = 0; j < length; j++)
+		for (int j = 0; j < coloumn; j++)
 		{
-			visitedPositions[i][j] = false;
+			visitedPositions[i][j] = 0;
 		}
 	}
-
 }
 
 double The_Maze::BestFact(pair<int, int> point)
 {
 	double fact = sqrt(pow((endingPoint.first - point.first), 2) + pow((endingPoint.second - point.second), 2));
-	return fact;
+	return -fact;
 }
 
 void The_Maze::BestFirst() {
-	zeroVisitedArray();
 
+	zeroVisitedArray();
 	priority_queue<pair<double, pair<int, int>>> searchQueue;
 	pair<double, pair<int, int>> start;
-	start.first = -1;
+
+	start.first = 0;
 	start.second = startingPoint;
+
 	visitedPositions[startingPoint.first][startingPoint.second] = true;
+
 	searchQueue.push(start);
+
 	parents[startingPoint.first][startingPoint.second] = { -1 , -1 };
+
 	int i = 0;
 	while (!searchQueue.empty())
 	{
+		i++;
 		pair<double, pair <int, int>> x = searchQueue.top();
 		searchQueue.pop();
+
 		if (x.second == endingPoint)
 		{
-			cout << "found at :: " << (x.second.second) / 2 << " " << (x.second.first) / 2 << endl;
-			bestTrack(x.second, 0);
+			pair<int, int>* path = new pair<int, int>[i + 1];
+			int length = bestTrack(x.second, 0, path);
+			printMethod(length, i, path, "Best First Search");
 			return;
 		}
+
 		node point(x.second.first, x.second.second);
 		getChildren(point, searchQueue);
 
 	}
 }
 
-void The_Maze::bestTrack(pair<int, int> point, int count)
+int The_Maze::bestTrack(pair<int, int> point, int count, pair<int, int> path[])
 {
 	pair<int, int> base = { -1,-1 };
 	if (parents[point.first][point.second] == base)
 	{
-		cout << "Path is " << count << " steps long" << endl;
-		cout << "(" << (point.first) / 2 << "," << (point.second) / 2 << ")" << " ";
-		return;
+		path[count] = point;
+		return count;
 	}
-	bestTrack(parents[point.first][point.second], count + 1);
-	cout << "(" << (point.second) / 2 << "," << (point.first) / 2 << ")" << " ";
-	return;
+	int x = bestTrack(parents[point.first][point.second], count + 1, path);
+	path[count] = point;
+	return x;
 }
 
 void The_Maze::getChildren(node point, priority_queue<pair<double, pair<int, int>>>& searchingQueue) {
@@ -146,6 +150,7 @@ void The_Maze::getChild(node point, int x, int y, priority_queue<pair<double, pa
 	{
 		visitedPositions[point.x + (x * 2)][point.y + (y * 2)] = true;
 
+
 		pair<int, int> newPoint;
 		newPoint.first = point.x + (x * 2);
 		newPoint.second = point.y + (y * 2);
@@ -161,24 +166,23 @@ void The_Maze::getChild(node point, int x, int y, priority_queue<pair<double, pa
 		searchingQueue.push(append);
 	}
 }
+
 void The_Maze::BFS()
 {
 	zeroVisitedArray();
-
 	int count = 0;
-	int z = 1, xAxis;
-	int x[4]{ 1,-1,0,0 }, y[4]{ 0,0,1,-1 };
-	queue<node> Node;
 
-	pair<int, pair<int, int>> ** Cells = new pair<int, pair<int, int>>*[length];
-	for (int i = 0; i < length; i++)
+	pair<int, pair<int, int>> ** Cells = new pair<int, pair<int, int>>*[row];
+	for (int i = 0; i < row; i++)
 	{
-		Cells[i] = new pair<int, pair<int, int>>[width];
+		Cells[i] = new pair<int, pair<int, int>>[coloumn];
 	}
+
 	node temp(startingPoint.first, startingPoint.second), temp2(0, 0);
 
 	Node.push(temp);
 	visitedPositions[temp.x][temp.y] = true;
+
 	Cells[temp.x][temp.y].first = 0;
 	Cells[temp.x][temp.y].second.first = -1;
 	Cells[temp.x][temp.y].second.second = -1;
@@ -188,68 +192,84 @@ void The_Maze::BFS()
 		temp = Node.front();
 		Node.pop();
 		visitedPositions[temp.x][temp.y] = true;
+
 		if (temp.x == endingPoint.first && temp.y == endingPoint.second)
 		{
-			pair<int, int> *BackTrack = new pair<int, int>[count];
-			visitedPositions[temp.x][temp.y] = true;
-
-			BackTrack[0].first = temp.x;
-			BackTrack[0].second = temp.y;
-			while (Cells[temp.x][temp.y].second.first != -1)
-			{
-				BackTrack[z].first = Cells[temp.x][temp.y].second.first;
-				BackTrack[z].second = Cells[temp.x][temp.y].second.second;
-				xAxis = temp.x;
-				temp.x = Cells[temp.x][temp.y].second.first;
-				temp.y = Cells[xAxis][temp.y].second.second;
-				z++;
-			}
-			cout << "Breadth First Search :  ";
-			for (int j = z; j >= 0; j = j - 2)
-			{
-				cout << " (" << BackTrack[j].first / 2 << "," << BackTrack[j].second / 2 << ") ";
-			}
-			cout << endl << "Length of Path: " << Cells[endingPoint.first][endingPoint.second].first / 2 << endl;
-			cout << "Number of Cells Visited: " << count / 2 << endl;
-
+			BFS_Found(Cells, temp, temp2, count);
 			break;
 		}
 		else {
-			for (int i = 0; i < 4; i++) {
-				if ((Maze[temp.x + x[i]][temp.y + y[i]] == ' ' || Maze[temp.x + x[i]][temp.y + y[i]] == 'E') &&
-					visitedPositions[temp.x + x[i]][temp.y + y[i]] == 0) {
-					Cells[temp.x + x[i]][temp.y + y[i]].first = Cells[temp.x][temp.y].first + 1;
-					Cells[temp.x + x[i]][temp.y + y[i]].second.first = temp.x;
-					Cells[temp.x + x[i]][temp.y + y[i]].second.second = temp.y;
-					temp2 = temp;
-					temp2.x += x[i];
-					temp2.y += y[i];
-					Node.push(temp2);
-				}
-			}
+			BFS_Helper(Cells, temp, temp2);
 		}
 		count++;
 	}
 }
 
+void The_Maze::BFS_Helper(pair<int, pair<int, int>> ** &Cells, node temp, node temp2) //Checks Neighbouring cells.
+{
+	int x[4]{ 1,-1,0,0 }, y[4]{ 0,0,1,-1 };
+	for (int i = 0; i < 4; i++) {
+		if ((Maze[temp.x + x[i]][temp.y + y[i]] == ' ' || Maze[temp.x + x[i]][temp.y + y[i]] == 'E') &&
+			visitedPositions[temp.x + x[i]][temp.y + y[i]] == 0)
+		{
+			Cells[temp.x + x[i]][temp.y + y[i]].first = Cells[temp.x][temp.y].first + 1;
+			Cells[temp.x + x[i]][temp.y + y[i]].second.first = temp.x;
+			Cells[temp.x + x[i]][temp.y + y[i]].second.second = temp.y;
+			temp2 = temp;
+			temp2.x += x[i];
+			temp2.y += y[i];
+			Node.push(temp2);
+		}
+	}
+}
+void The_Maze::BFS_Found(pair<int, pair<int, int>> ** &Cells, node temp, node temp2, int count)
+{
+	int z = 0, xAxis;
+	pair<int, int> *BackTrack = new pair<int, int>[count];
+	visitedPositions[temp.x][temp.y] = true;
+
+	BackTrack[0].first = temp.x;
+	BackTrack[0].second = temp.y;
+	while (Cells[temp.x][temp.y].second.first != -1)
+	{
+		BackTrack[z].first = Cells[temp.x][temp.y].second.first;
+		BackTrack[z].second = Cells[temp.x][temp.y].second.second;
+		xAxis = temp.x;
+		temp.x = Cells[temp.x][temp.y].second.first;
+		temp.y = Cells[xAxis][temp.y].second.second;
+		z++;
+	}
+
+	pair<int, int> *BackTrack2 = new pair<int, int>[z / 2 + 1];
+	int iterator = z / 2;
+	for (int j = z; j >= 0; j = j - 2)
+	{
+
+		BackTrack2[iterator] = BackTrack[j];
+		iterator--;
+	}
+
+	printMethod(z / 2, count / 2, BackTrack2, "Breadth First Search");
+
+}
 void The_Maze::dfs()
 {
-	
-
-	
 	zeroVisitedArray();
-	dfs_help(startingPoint.first,startingPoint.second);
+	dfs_help(startingPoint.first, startingPoint.second);
+	pair<int, int>* path = new pair<int,int>[s.size()+1] ;
+	int i = s.size()-1;
 	while (s.empty() != true)
 	{
-		cout << "(" << s.top().second/2 << " , " << s.top().first/2 << ")";
+		path[i].first = s.top().first;
+		path[i].second = s.top().second;
 		s.pop();
+		i--;
 	}
-	cout << endl << "Number of cells visited: " << dfs_steps_ctr << endl;
-	cout << "Number of steps of the answer:  " << dfs_path_ctr << endl;
+	printMethod(dfs_path_ctr, dfs_steps_ctr, path, "Depth First Search");
 }
 
 
-bool The_Maze::dfs_help(int i,int j)
+bool The_Maze::dfs_help(int i, int j)
 {
 	visitedPositions[i][j] == true;
 	pair<int, int>temp;
@@ -259,7 +279,7 @@ bool The_Maze::dfs_help(int i,int j)
 	{
 		dfs_steps_ctr++;
 		s.push(temp);
-		dfs_path_ctr++;
+		//dfs_path_ctr++;
 		return true;
 	}
 	if (Maze[i + 1][j] == ' '&&visitedPositions[i + 2][j] != true)
@@ -275,8 +295,6 @@ bool The_Maze::dfs_help(int i,int j)
 			return true;
 		}
 	}
-	
-	 
 	if (Maze[i][j + 1] == ' '&&visitedPositions[i][j + 2] != true)
 	{
 		visitedPositions[i][j + 2] = true;
@@ -299,9 +317,17 @@ bool The_Maze::dfs_help(int i,int j)
 			return true;
 		}
 	}
-
 	return false;
-	
+}
 
-
+void The_Maze::printMethod(int length, int visited, pair<int, int>path[], string method)
+{
+	cout << "Using " << method << " : ";
+	cout << "Path Length :" << length << endl << endl;
+	for (int i = length; i >= 0; i--)
+	{
+		cout << "(" << (path[i].second) / 2 << "," << (path[i].first) / 2 << ")" << " ";
+	}
+	cout << endl << endl << "Total visited Cells :" << visited << endl;
+	cout << endl << "***********************************************************************************************************************\n\n" << endl;
 }
