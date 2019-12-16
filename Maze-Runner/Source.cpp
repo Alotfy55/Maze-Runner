5c832ea0ba475d560c44b02190510fa8bf70ef70
YoussefGabry <58152227+YoussefGabry@users.noreply.github.com>
2019-12-16 17:13:01 +0200

    Update Source.cpp

--- Maze-Runner/Source.cpp
+++ Maze-Runner/Source.cpp
@@ -1,33 +1,11 @@
 #include "classes.h"
-
+#include<string>
 using namespace std;
 
 int main() {
-
-	The_Maze Maze;
-	cout << "Welcome to MazeRunner\n"; 
-	while (true) {
-		int choice;  
-		cout << "Press 1 to choose a maze from the file\n";
-		cout << "Press 2 to Generate a Maze\n"; 
-		cin >> choice; 
-		if (choice == 1) {
-			string Mazename; 
-			cout << "Enter the file name : "; 
-			cin >> Mazename; 
-			Maze.takeInput(Mazename); 
-			Maze.BestFirst();
-			Maze.BFS();
-			Maze.dfs();
-		}
-		else if (choice == 2) {
-			Maze.GenerateMaze(); 
-		}
-		Maze.printMaze();
-		Maze.BestFirst();
-		Maze.BFS();
-		Maze.dfs(); 
-		 
-	}
+	The_Maze maze;
+	maze.takeInput("Maze");
+	maze.printMaze();
+	maze.DijkstraSearch();
 	return 0;
 }
